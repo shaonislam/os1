bf041cc7f129f76a67b87bdb6cd05be9ae92ada7
Shaon Islam <sidw8@mail.umsl.edu>
2018-09-06 18:16:54 -0500

    Trying to implement getopt with just h, but warning with implicit declaration

--- chain.c
+++ chain.c
@@ -4,11 +4,61 @@
 #include <sys/types.h>
 
 
-int main (int argc, char *argv[]) {
+
+/* User getopt: 
+	 -n x:
+	 -h: display help message indicating type of input needed
+	 -p: uses perror to generate a test error message as describes in task4
+*/
+
+
+
+
+
+
+int main (int argc, char *argv[]) 
+{
 	pid_t childpid = 0;
-	int i, n;
- 	
-	if (argc != 2){ /* check for valid number of command-line arguments */
+	int i, n; 	
+	int helpflag;
+	int errorflag;
+	char option;
+
+	/* Attempt 1 at get opt */
+	/* optopt: when getopt stumbles on an unknown option char or with a missing arg */
+
+
+	option = getopt (argc, argv, "h");
+
+	while(option != -1)
+	{
+		switch (option)
+			{
+			case 'h':
+				helpflag = 1;
+				break;	
+			case '?':
+				errorflag = 1;
+				break;
+
+			}
+	}
+
+
+	if (helpflag == 1)
+	{
+		fprintf(stderr, "HELP MESSAGE");
+	}
+
+	if (errorflag == 1)
+        {
+                fprintf(stderr, "ERROR  MESSAGE");
+        }
+
+
+	if (argc != 2)
+	{ 
+		/* check for valid number of command-line arguments */
  		fprintf(stderr, "Usage: %s processes\n", argv[0]);
  		return 1;
  	}
